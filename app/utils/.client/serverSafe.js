d32f5f81487734a76165a3b07784f022cb612ffa
Robert Ingrum <robert.ingrum@shopify.com>
2026-04-05 21:53:56 -0600

    Wipe out old code.

--- app/utils/.client/serverSafe.js
+++
@@ -1,7 +0,0 @@
-// This should create a server-safe version of this library since this is in the .client dir. Will return undefined on the server.
-import * as ReactXTermJs from 'react-xtermjs';
-import { FitAddon } from '@xterm/addon-fit';
-import { WebLinksAddon } from '@xterm/addon-web-links';
-
-export { ReactXTermJs, FitAddon, WebLinksAddon };
-
